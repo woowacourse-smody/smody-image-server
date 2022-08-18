f9a69c401f31ab7b83055d035e03abf80577ce2c
jojogreen91 <iawbg13@gmail.com>
2022-08-18 11:39:51 +0900

    fix: cors origin 변경

--- src/main/java/smody/smodyimageserver/Config.java
+++ src/main/java/smody/smodyimageserver/Config.java
@@ -17,7 +17,7 @@ public class Config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/images/upload")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET");
+                .allowedOrigins("https://www.smody.co.kr", "https://test.smody.co.kr")
+                .allowedMethods("POST");
     }
 }
